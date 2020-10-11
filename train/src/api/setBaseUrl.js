6db8d1cbb15334a9a1e926ab54e04f261572990d
bloomlj <kollocc@live.com>
2020-10-11 21:01:59 +0800

    修改后端的地址

--- train/src/api/setBaseUrl.js
+++ train/src/api/setBaseUrl.js
@@ -2,10 +2,10 @@ var baseUrl = ''
 
 switch (process.env.NODE_ENV) {
   case 'development':
-    baseUrl = 'http://stooop.top/'
+    baseUrl = 'http://api.stooop.top/'
     break
   case 'production':
-    baseUrl = 'http://stooop.top/'
+    baseUrl = 'http://api.stooop.top/'
     break
 }
 
